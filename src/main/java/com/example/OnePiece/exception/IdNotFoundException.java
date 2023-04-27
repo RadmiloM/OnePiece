abce2a271e8e6c386b643abfb5c21eb5f887bc5b
RadmiloM <markovicradmilo35@gmail.com>
2023-04-27 10:35:48 +0200

    GlobalExceptionHandler and IdNotFoundException created.

---
+++ src/main/java/com/example/OnePiece/exception/IdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.OnePiece.exception;
+
+public class IdNotFoundException extends RuntimeException {
+
+    public IdNotFoundException(String message){
+        super(message);
+    }
+}
