47c5fe4450455d6f28020176e2b5ad1b72ea2493
RadmiloM <markovicradmilo35@gmail.com>
2023-04-27 10:36:28 +0200

    OnePieceDTO created.

---
+++ src/main/java/com/example/OnePiece/dto/OnePieceDTO.java
@@ -0,0 +1,21 @@
+package com.example.OnePiece.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OnePieceDTO {
+
+    private String name;
+
+    private int age;
+
+    private String ability;
+
+    private int strength;
+
+    private String crew;
+}
